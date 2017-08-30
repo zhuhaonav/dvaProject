660ab98e507bd9678e0e60037bcd94c4ace6c5b3
朱昊 <781984548@qq.com>
2017-08-30 15:58:33 +0800

    commit

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
-import Login from './routes/Login';
+import IndexPage from './routes/IndexPage'
 import Products from './routes/Products';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={Login} />
+      <Route path="/" component={IndexPage} />
       <Route path="/products" component={Products} />
     </Router>
   );
