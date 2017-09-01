038a1f4f5ba638926c31f8f53daa518996d461a0
朱昊 <781984548@qq.com>
2017-09-01 18:21:00 +0800

    tijiao

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import { Button, Row, Form, Input } from 'antd';
-const FormItem = Form.Item
-const Login = ({
-  app,
-  dispatch,
-}) => {
-    const {loginLoading} = app;
-    const uri = 'https://t.alipayobjects.com/images/T1QUBfXo4fXXXXXXXX.png'
-    return(
-      <div style={styles.login}>
-        <div style={styles.loginView}>
-          <div style={styles.loginHead}>
-            <img style={styles.loginImg} src={uri}></img>
-            <span style={styles.loginText}>Dva Login</span>
-          </div>
-          <div style={styles.loginBody}>
-            <form style={{width:'80%',}}>
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import query from '../services/example';
 
-            </form>
+const FormItem = Form.Item
+class Logins extends React.Component{
+
+    handleSubmit = (e) => {//提交表单
+      e.preventDefault();
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+          query(values);
+          //location.href = location.origin+'#/main'
+        }
+      });
+    }
+    render(){
+      const uri = 'https://t.alipayobjects.com/images/T1QUBfXo4fXXXXXXXX.png'
+      const { getFieldDecorator } = this.props.form;
+      return(
+        <div style={styles.login}>
+          <div style={styles.loginView}>
+            <div style={styles.loginHead}>
+              <img style={styles.loginImg} src={uri}></img>
+              <span style={styles.loginText}>Dva Login</span>
+            </div>
+            <div style={styles.loginBody}>
+              <Form style={{width:'80%',}}>
+                <FormItem>
+                  {getFieldDecorator('username', {
+                    rules: [{required: true}],
+                  })(<Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />)}
+                </FormItem>
+                <FormItem>
+                  {getFieldDecorator('password', {
+                    rules: [{required: true}],
+                  })(<Input prefix={<Icon type="password" style={{ fontSize: 13 }} />} placeholder="Password" />)}
+                </FormItem>
+                <Button style={styles.loginButton} type="primary" size="large" onClick={this.handleSubmit}>
+                  登录
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
 }
-
-Login.propTypes = {
+const Login = Form.create()(Logins);
+Login.propTypes = {//检测数据类型
   form: PropTypes.object,
   app: PropTypes.object,
   dispatch: PropTypes.func,
@@ -41,7 +65,7 @@ const styles = {
   },
   loginView:{
     width:320,height:320,
-    boxShadow:'0 0 100px rgba(0,0,0,.08)',
+    boxShadow:'0 0 100px rgba(0,0,0,.1)',
   },
   loginHead:{
     width:320,
